42a7b34db50ba1ee07aa53ed8091475e20b30015
Tigerfriend1 <kimtg1997@pusan.ac.kr>
2025-06-11 14:52:18 +0900

    타입스크립트 배열, 클래스, 인터페이스, 제네릭 연습

--- ch03/dist/main6.js
+++ ch03/dist/main6.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
     날짜 : 2025.06.11
     이름 : 김태경
@@ -14,24 +15,20 @@
      > npx tsc
      > npx tsc --watch
 */
-
 console.log("1) Hello ______________________________");
-let hello: string = "Hello TypeScript!!";
+let hello = "Hello TypeScript!!";
 console.log(hello);
-
 console.log("2) 기본타입 ______________________________");
-
-let value1: number = 1;
-let value2: number = 1.2;
-let value3: boolean = true;
-let value4: string = "A";
-let value5: string = "Hello";
-let value6: undefined = undefined;
-let value7: null = null;
-let value8: any = 1;
+let value1 = 1;
+let value2 = 1.2;
+let value3 = true;
+let value4 = "A";
+let value5 = "Hello";
+let value6 = undefined;
+let value7 = null;
+let value8 = 1;
 value8 = "any value";
-let value9: unknown = "greeting";
-
+let value9 = "greeting";
 if (typeof value9 === "string") {
   value9 = value9.toUpperCase();
 }
@@ -44,98 +41,71 @@ console.log(`value6 : ${value6}`);
 console.log(`value7 : ${value7}`);
 console.log(`value8 : ${value8}`);
 console.log(`value9 : ${value9}`);
-
 console.log("3) 배열과 튜플 ______________________________");
 // 배열
-let list1: number[] = [1, 2, 3];
-let list2: string[] = ["apple", "banana", "cherry"];
-
+let list1 = [1, 2, 3];
+let list2 = ["apple", "banana", "cherry"];
 // 튜플 : 타입과 갯수가 정해진 배열
-let t1: [string, number] = ["hello", 11];
-
+let t1 = ["hello", 11];
 console.log(`list1 : ${list1}`);
 console.log(`list2 : ${list2}`);
 console.log(`t1 : ${t1}`);
-
 console.log("4) 함수 ______________________________");
-function plus(value1: number, value2: number): number {
+function plus(value1, value2) {
   return value1 + value2;
 }
-function greeting(name: string): string {
+function greeting(name) {
   return `Hello~ ${name}`;
 }
-function welcome(): void {
+function welcome() {
   console.log("welcome void~");
 }
 // never :함수가 정상종료가 아닌 예외가 발생하는 함수 반환 타입.
-function error(message: string): never {
+function error(message) {
   throw new Error(message);
 }
 console.log(plus(1, 2));
 console.log(greeting("동글동글"));
 welcome();
 //error("예외발생~~!!!");
-
 console.log("5) 클래스 ______________________________");
 class Person {
-  public name: string;
-  private age: number;
-
-  constructor(name: string, age: number) {
+  constructor(name, age) {
     this.name = name;
     this.age = age;
   }
-
-  public get getName(): string {
+  get getName() {
     return this.name;
   }
-  hello(): void {
+  hello() {
     console.log(`${this.name}, ${this.age}`);
   }
 }
-
-const hong: Person = new Person("글이동", 21);
+const hong = new Person("글이동", 21);
 hong.hello();
-
 console.log("6) 상속 ______________________________");
 console.log("7) 인터페이스 ______________________________");
-interface User {
-  name: string;
-  age: number;
-}
-const user1: User = {
+const user1 = {
   name: "동홍글",
   age: 23,
 };
-
 console.log(user1);
-
-interface Vehicle {
-  brand: string;
-  drive(): void;
-}
-
-class Car implements Vehicle {
-  brand: string;
-  constructor(brand: string) {
+class Car {
+  constructor(brand) {
     this.brand = brand;
   }
-  drive(): void {
+  drive() {
     console.log(`im ${this.brand}`);
   }
 }
-
-const bmw: Car = new Car("BMW");
+const bmw = new Car("BMW");
 bmw.drive();
-
 console.log("8) 제네릭 ______________________________");
-function identity<T>(arg: T): T {
+function identity(arg) {
   return arg;
 }
-
-const result1: string = identity<string>("hello");
-const result2: number = identity<number>(1);
+const result1 = identity("hello");
+const result2 = identity(1);
 console.log(result1);
 console.log(result2);
-
 console.log("9) 기타타입 ______________________________");
